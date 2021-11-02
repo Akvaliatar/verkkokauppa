d48a835f2d7e9921b3bf249bf22015c4f6c8eacc
Valtpit <77121949+Valtpit@users.noreply.github.com>
2021-11-02 14:59:33 +0200

    first push
    frontin alku gittiin

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App () {
+  return (
+    <div>
+      <header>
+        <div>
+          <p>Tässä meidän hieno header</p>
+        </div>
+      </header>
+
+      <div>
+        <p>kaiken mailman sisältöä</p>
+      </div>
+
+      <footer>
+        <p>ja vielä hieno footer</p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
