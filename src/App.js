f7ca135e01a7b981cba1bd9214d348cc72c04689
Valtpit <valtpit@gmail.com>
2021-11-05 16:30:18 +0200

    etusivun aloitus

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App () {
     <div>
       <header>
         <div>
-          <p>Tässä meidän hieno header</p>
+          <a href=''>FI</a>|<a href=''>EN</a>
+          <figure>
+            <img scr='./logo.jpeg' alt='logo'></img>
+          </figure>
+        </div>
+        <div>
+          <a href=''>Koti</a>
+          <a href=''>Meistä</a>
+          <a href=''>Palvelut</a>
+          <a href=''>Verkkokauppa</a>
+          <a href=''>Omat tiedot</a>
         </div>
       </header>
 
@@ -14,10 +24,29 @@ function App () {
       </div>
 
       <footer>
-        <p>ja vielä hieno footer</p>
+        <div>
+            <div>
+              <p>Seuraa meitä</p>
+              <figure>
+                <img scr='' alt=''></img>
+                <img scr='' alt=''></img>
+              </figure>
+              <a href=''>evästeet</a>
+              <a href=''>rekisteriseloste</a>
+              <a href=''>toimitusehdot</a>
+              <a href=''>yhteystiedot</a>
+            </div>
+            <div>
+              <p>Lataa sovellus</p>
+            </div>
+        </div>
       </footer>
     </div>
+
+    
   )
 }
 
+
 export default App
+
