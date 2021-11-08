c54d2171470031057de21042489e09bae4610d8c
Valtpit <valtpit@gmail.com>
2021-11-08 13:04:57 +0200

    Navbar

--- src/Header.js
+++ src/Header.js
@@ -3,29 +3,23 @@ import { Link } from "react-router-dom"
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-export default function Header() {
-    return (
-        <header>
-        <Row>
-          <Col>
-            <a href=''>FI</a>|<a href=''>EN</a>
-          </Col>
-          <Col>
-            <figure>
-              <img scr={'./logo.png'} alt='logo' />
-            </figure>
-          </Col>
-          <Col>
-            <p>ostoskori icon</p>
-          </Col>
-        </Row>
-        <div>
-          <a href='/'>Koti</a>
-          <a href='/aboutus'>Meistä</a>
-          <a href='/services'>Palvelut</a>
-          <a href='/Shop'>Verkkokauppa</a>
-          <a href='/about'>Omat tiedot</a>
-        </div>
-      </header>
-    )
+export default function Header () {
+  return (
+    <header>
+      <Row>
+        <Col>
+          <a href=''>FI</a>|<a href=''>EN</a>
+        </Col>
+        <Col>
+          <figure>
+            <img scr={'./logo.png'} alt='logo' />
+          </figure>
+        </Col>
+        <Col>
+          <p>ostoskori icon</p>
+        </Col>
+      </Row>
+      
+    </header>
+  )
 }
