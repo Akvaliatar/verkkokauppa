3f729adb3ed39ae4b1ffc36d2462f8be3d9a9eab
Aura <n0hiau00@students.oamk.fi>
2021-11-10 12:35:36 +0200

    app.css: footerin muotoilut omaan tiedostoon, kommentteja tunnistamisen takia (muistakaa tarkentaa mikä class on millekin),footer.js ja css: ikonien asettelua

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,44 +1,66 @@
-import React from 'react'
+import React from "react";
+import "../css/footer.css";
 import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { SiFacebook, SiInstagram, SiGoogleplay, SiApple } from "react-icons/si";
 
-export default function Footer () {
+export default function Footer() {
   return (
     <footer>
       <Container>
         <Row>
           <Col>
-            <ul className='footer_ul'>
-              <li>
-                <a href=''>evästeet</a>
-              </li>
-              <li>
-                <a href=''>rekisteriseloste</a>
-              </li>
-              <li>
-                <a href=''>toimitusehdot</a>
-              </li>
-              <li>
-                <a href=''>yhteystiedot</a>
-              </li>
-            </ul>
+            <div>
+              <ul>
+                <li>
+                  <a href="">evästeet</a>
+                </li>
+                <li>
+                  <a href="">rekisteriseloste</a>
+                </li>
+                <li>
+                  <a href="">toimitusehdot</a>
+                </li>
+                <li>
+                  <a href="">yhteystiedot</a>
+                </li>
+              </ul>
+            </div>
           </Col>
           <Col>
-          <h3>Seuraa meitä</h3>
-            <p> <Link to="https://www.instagram.com/toivon_kotielainpuisto/"><SiInstagram /></Link><Link to ="https://www.facebook.com/profile.php?id=100074719422748"><SiFacebook /></Link></p>
+            <h3>Seuraa meitä</h3>
+            <div className="ikonit">
+              <a>
+                <Link to="https://www.instagram.com/toivon_kotielainpuisto/">
+                  <SiInstagram size={28} />
+                </Link>
+              </a>
+              <a>
+                <Link to="https://www.facebook.com/profile.php?id=100074719422748">
+                  <SiFacebook size={28} />
+                </Link>
+              </a>
+            </div>
           </Col>
           <Col>
             <h3>Lataa sovellus</h3>
-            <ul className="footer_ul">
-                <li><SiGoogleplay /></li>
-                <li><SiApple /></li>
-              </ul>
+            <div className="ikonit">
+              <a>
+                <Link to="https://play.google.com/store/apps?hl=fi&gl=US">
+                  <SiGoogleplay size={28} />
+                </Link>
+              </a>
+              <a>
+                <Link to="https://apps.apple.com/us/app/apple-store/id375380948">
+                  <SiApple size={28} />
+                </Link>
+              </a>
+            </div>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
+  );
 }
