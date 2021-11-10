f4672048b2bdd4d2ffd9f7f868de4cdbc14c8d09
Aura <n0hiau00@students.oamk.fi>
2021-11-10 10:25:54 +0200

    ostoskorin ikonin asettelu ja koon muuttaminen, nav about valmis (keskitetty), app.css sai lisäyksen h1-muotoiluun

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/navbar.css";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BsCart4 } from "react-icons/bs";
@@ -10,10 +10,11 @@ import Logo from "../img/logo.png";
 
 export default function NavBar() {
   return (
-    <Container fluid>
-      <Container>
+    <div className="navi">
+      <Container fluid className="mx-auto">
         <Row>
-            <Col className="kieli">
+          <Col className="kieli">
+            <div>
               <a href="" className="FI">
                 FI
               </a>
@@ -21,60 +22,48 @@ export default function NavBar() {
               <a href="" className="EN">
                 EN
               </a>
-            </Col>
-            <Col>
-              <figure>
-                <Link to="">
-                  <img src={Logo}/>
-                </Link>
-              </figure>
-            </Col>
-            <Col>
-              <p>
-                <BsCart4 />
-              </p>
-            </Col>
-        </Row>
-      </Container>
-      <Navbar className="navbar" expand="lg">
-      <Container>
-        <Row>
-        
-          <Col xs={12}>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/aboutus">Meistä</Nav.Link>
-                <Nav.Link href="/services">Palvelut</Nav.Link>
-                <Nav.Link href="/shop">Verkkokauppa</Nav.Link>
-                <Nav.Link href="/about">Omat tiedot</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+            </div>
+          </Col>
+          <Col>
+            <figure>
+              <Link to="">
+                <img src={Logo} />
+              </Link>
+            </figure>
+          </Col>
+          <Col>
+            <p className="ostoskori">
+              <BsCart4 size={38} />
+            </p>
           </Col>
         </Row>
       </Container>
+      <Navbar className="navbar" expand="lg">
+        <Container>
+          <Col>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto">
+                <Nav.Link href="/">
+                  <h3>Home</h3>
+                </Nav.Link>
+                <Nav.Link href="/aboutus">
+                  <h3>Meistä</h3>
+                </Nav.Link>
+                <Nav.Link href="/services">
+                  <h3>Palvelut</h3>
+                </Nav.Link>
+                <Nav.Link href="/shop">
+                  <h3>Verkkokauppa</h3>
+                </Nav.Link>
+                <Nav.Link href="/about">
+                  <h3>Omat tiedot</h3>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Container>
       </Navbar>
-    </Container>
+    </div>
   );
 }
-
-<Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>;
