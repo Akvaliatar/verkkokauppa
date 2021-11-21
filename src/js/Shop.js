d7804b030b187136553a7526ddcdb684e17f6e02
Valtpit <77121949+Valtpit@users.noreply.github.com>
2021-11-21 20:35:50 +0200

    back-end haku kategorioille

--- src/js/Shop.js
+++ src/js/Shop.js
@@ -2,8 +2,24 @@ import { useEffect, useState } from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import DogandCat from '../img/dogandcat.jpg';
 import Logo from '../img/logo.png';
+import axios from "axios"
 
-export default function Shop () {
+export default function Shop() {
+
+  const CATEGORY = "http://localhost/kotielainpuisto/products/getcategories.php"
+  const [item, setItem] = useState([])
+  const [tuoteryhma, setTuoteryhma] = useState("")
+
+  useEffect(() => {
+    axios.get(CATEGORY)
+      .then((response) => {
+        setItem(response.data)
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }, [])
+
+
 
   const [showButton, setShowButton] = useState(false);
 
@@ -27,10 +43,10 @@ export default function Shop () {
   return (
     <Container className="shopContainer">
       <Row>
-        <Col xs={{span: 12, order: 2}} lg={{span: 12, order: 1}}>
+        <Col xs={{ span: 12, order: 2 }} lg={{ span: 12, order: 1 }}>
           <h1>Shop</h1>
         </Col>
-        <Col xs={{span: 12, order: 1}} lg={{span: 12, order: 1}}>
+        <Col xs={{ span: 12, order: 1 }} lg={{ span: 12, order: 1 }}>
           <p>Alta löydät omista kategorioista myytävänä olevia tuotteita ja palveluita.</p>
         </Col>
       </Row>
@@ -39,7 +55,7 @@ export default function Shop () {
         <Col xs={{ span: 12, order: 1 }} md={{ span: 6, order: 1 }} lg={{ span: 4, order: 1 }} >
           <Card style={{ width: '18rem', border: " 2px solid #514b3b" }}>
             <Card.Img variant="top" src={DogandCat} />
-            <Card.Body style={{ background: "#a8ae8a"}}>
+            <Card.Body style={{ background: "#a8ae8a" }}>
               <Card.Title>Eläin herkut</Card.Title>
               <Card.Text>
                 Täältä voit katsella Toivon kotieläinpuiston eläinten lempi herkkuja ja voit ostelle herkkuja omalle lenmmikille.
@@ -47,14 +63,14 @@ export default function Shop () {
               <div className="buttonToCenter" >
                 <button className="webShopButton"><a href="#">Katso lisää</a></button>
               </div>
-              
+
             </Card.Body>
           </Card>
         </Col>
         <Col xs={{ span: 12, order: 2 }} md={{ span: 6, order: 2 }} lg={{ span: 4, order: 2 }} >
           <Card style={{ width: '18rem', border: " 2px solid #514b3b" }} >
             <Card.Img variant="top" src={"https://koulu-b8d54.web.app/kissa.jpg"} /> {/* testattu että linkitys toimii toiselta sivulta */}
-            <Card.Body style={{ background: "#a8ae8a"}} >
+            <Card.Body style={{ background: "#a8ae8a" }} >
               <Card.Title>Eläinten hoitotuotteet</Card.Title>
               <Card.Text>
                 Kategoriasta löydät eläinystävällisiä hoitotuotteita.
@@ -123,13 +139,33 @@ export default function Shop () {
         </Col>
       </Row>
 
+
+
+      {item?.map(item => (
+        <div key={item.trnro}>
+            <Card style={{ width: '18rem', border: " 2px solid #514b3b" }}>
+              <Card.Img variant="top" src={DogandCat} />
+              <Card.Body style={{ background: "#a8ae8a" }}>
+                <Card.Title><p>{item.trnimi}</p></Card.Title>
+                <Card.Text>
+                  <p>{item.teksti}</p>
+                </Card.Text>
+                <div className="buttonToCenter" >
+                  <button className="webShopButton"><a href="#">Katso lisää</a></button>
+                </div>
+              </Card.Body>
+            </Card>
+ 
+        </div>
+      ))}
+
       {showButton && (
         <button onClick={scrollToTop} className="back-to-top">
           &#8679;
         </button>
       )}
     </Container>
-    
+
 
   )
 }
