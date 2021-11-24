2eb2bed65b0328172b3e0aa3043a7fe9c22978ee
Aura <n0hiau00@students.oamk.fi>
2021-11-24 18:27:50 +0200

    navbar.css sai pienen lisäyksen tunnistautumisen muotoiluun, lisätty  product.js ja -css,shop.css, category.css

---
+++ src/js/Product.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../css/product.css";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Product() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
